8113d458ed8730abcdca3ef4aeebb2246664fa7d
Laraib39 <sheikhlareb9d@gmail.com>
2020-07-30 14:25:29 -0700

    ready to commit

---
+++ questions.js
@@ -0,0 +1,110 @@
+var quiz = [{
+        // id:1,
+        question: "Where is Faisalabad located in Pakistan?",
+        answer: 1,
+        options: [
+            "Sindh",
+            "Punjab",
+            "Balochistan",
+            "KPK"
+        ]
+    },
+    {
+        //id:2,
+        question: "What is the Full form of KPK?",
+        answer: 0,
+        options: [
+            "Khyber Pakhtun Khwua",
+            "Khyber Pakhtun Karachi",
+            "Khyber Pak Khwua",
+            "Khwua Pakhtun Khyber",
+        ]
+    },
+    {
+        //id:3,
+        question: "What is the result of 23 + 7?",
+        answer: 2,
+        options: [
+            "31",
+            "29",
+            "30",
+            "28",
+        ]
+    },
+    {
+        //id:4,
+        question: "Which is the 9th month of year?",
+        answer: 0,
+        options: [
+            "September",
+            "November",
+            "October",
+            "December",
+        ]
+    },
+    {
+        //id:5,
+        question: "Who was the first Governor General of Pakistan?",
+        answer: 2,
+        options: [
+            "Sir Syed Ahmed Khan",
+            "Liaquat Ali Khan",
+            "Quaid e Azam Mohammad Ali Jinnah",
+            "Moulana Shoukat Ali Johar",
+        ]
+    },
+    {
+        //id:6,
+        question: "What is the result of 33 * 3?",
+        answer: 1,
+        options: [
+            "89",
+            "99",
+            "96",
+            "98",
+        ]
+    },
+    ]
+
+
+
+
+
+
+
+
+
+//window.onload = function(){
+//     show(0)
+// }
+// 
+// quesCount = 0;
+// function nextBtn(){
+//     quesCount++;
+//     console.log(quesCount);
+//     show(quesCount);
+// }
+
+// function show(e){
+//     var ques = document.getElementById("question");
+//     ques.innerHTML = '<h5>' ${questions[e].question}'</h5>
+//     '<ul class="list">'
+//         <li class="option">${questions[e].options[0]}</li>
+//         <li class="option">${questions[e].options[1]}</li>
+//         <li class="option">${questions[e].options[2]}</li>
+//         <li class="option">${questions[e].options[3]}</li>'</ul>'
+    
+    // Active();
+
+// function Active(){
+//     var option1 = document.querySelectorAll("li.option").innerHTML;
+//     for(var i=0; i < Option.length; i++){
+//         option1[i].onlick = function(){
+//             for(var j=0; j < Option.length; j++)  
+//             if(option1[j].classList.contains("active")){
+//                 option1[j].classList.remove("active")
+//             }
+//         }
+//         option1[i].classList.add("active")
+//     }
+// }
